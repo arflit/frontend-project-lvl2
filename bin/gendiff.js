0069fb88aae64e8dee45c52c23f6238ef14858c9
Arkady Flitman <arflit@gmail.com>
2021-11-07 22:12:17 +0300

    feat: eslint added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
